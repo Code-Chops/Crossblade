8f01d54b896d525e47e23e30ec97ba85d1043f3f
Max Bergman <maxbergman@outlook.com>
2023-05-08 21:37:06 +0200

    1.0.3: Added TransitionTimingFunction and Position.

---
+++ Crossblade/Position.cs
@@ -0,0 +1,20 @@
+﻿namespace CodeChops.Crossblade;
+
+public enum Position
+{
+    Static,
+    Relative,
+    Absolute,
+    Sticky,
+    Fixed,
+    Initial,
+    Inherit,
+    Unset,
+    Revert,
+}
+
+public static class PositionExtensions
+{
+    public static string GetAttribute(this Position position) 
+        => position.ToString().ToLowerInvariant();
+}
